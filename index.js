e3db9f46aeb7c4fc85e433407126280c3a89fde2
Sergej <sfelde3@gmail.com>
2024-10-23 17:33:53 +0200

    auf Aktueller Stand gebracht, Feller im index.js korrigiert.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
+ 
+import pool from './db.js';
+ 
 const app = express();
  
 // EJS als View-Engine einrichten
 app.set('view engine', 'ejs');
+
+// Middleware, um URL-encoded-Daten zu verarbeiten
+app.use(express.urlencoded({ extended: true }));
+
  
 // Route mit dynamischem Inhalt
 app.get('/', (req, res) => {
@@ -12,12 +20,32 @@ app.get('/', (req, res) => {
 
  
 // Weitere Route
+app.get('/register', (req, res) => {
+  res.render('register', { title: 'Registrierung' });
+});
 app.get('/about', (req, res) => {
   res.send('Dies ist die About-Seite.');
 });
 app.get('/contact', (req, res) =>{
     res.send('Kontaktiere mich mal!!!')
-}) 
+}); 
+app.post('/register', async (req, res) => {
+  console.log(req.body);
+  const { username, name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const conn = await pool.getConnection();
+ 
+  try {
+    await conn.query('INSERT INTO users (username, name, email, password_hash) VALUES (?, ?, ?, ?)',
+      [username, name, email, hashedPassword]);
+    res.status(201).redirect('/');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Fehler bei der Registrierung');
+  } finally {
+    conn.release();
+  }
+});
 // Server starten
 app.listen(3000, () => {
   console.log('Server läuft auf http://localhost:3000');
--- secrets.js
+++ secrets.js
@@ -2,7 +2,7 @@ const secrets = {
     db_server: 'localhost',
     db_username: 'sf',
     db_password: '08111981',
-    db_database: 'todo_list'
+    db_database: 'project_db'
 }
 
 export default secrets;
