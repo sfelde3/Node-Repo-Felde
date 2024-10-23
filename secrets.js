0da48e55236e09385d42e3f552d8f76336884fc8
Sergej <sfelde3@gmail.com>
2024-10-23 21:28:41 +0200

    Logging eingefügt

--- secrets.js
+++ secrets.js
@@ -2,7 +2,8 @@ const secrets = {
     db_server: 'localhost',
     db_username: 'sf',
     db_password: '08111981',
-    db_database: 'project_db'
+    db_database: 'project_db',
+    jwt_secret_key : '053c0c1da88ec3f3fbb79f9467738f637cc327cf'
 }
 
 export default secrets;
